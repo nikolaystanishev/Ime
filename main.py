03a998a5153077d8c87c5370337231b25f900aef
IvoTod <ivo.tod.todorov@gmail.com>
2018-03-16 17:13:29 +0200

    Add textboxes for UI

--- main.py
+++ main.py
@@ -14,8 +14,10 @@ def update(ticks):
 run_app = True
 last_frame_time = pygame.time.get_ticks()
 test1 = TextBox(0, 0, 10, 10, "test")
+test2 = TextBox(20, 5, 20, 5, "test")
 while run_app:
-    event_here = tb.poll_event()
+    tb.clear()
+    event_here = tb.peek_event()
     while event_here:
         (type, ch, key, mod, w, h, x, y) = event_here
         if type == termbox.EVENT_KEY:
@@ -23,7 +25,8 @@ while run_app:
             run_app = False
         event_here = tb.peek_event()
     current_frame_ticks = pygame.time.get_ticks() - last_frame_time
-    tb.clear()
+    test1.draw(tb)
+    test2.draw(tb)
     update(current_frame_ticks)
     tb.present()
 
--- textbox.py
+++ textbox.py
@@ -2,17 +2,29 @@ import termbox
 from ui_element import UIElement
 
 class TextBox(UIElement):
-    def __init__(self, x, y, w, h, text, borderSym=219):
+    def __init__(self, x, y, w, h, text, border_sym=termbox.WHITE):
         super().__init__(x, y)
         self.w = w
         self.h = h
         self.text = text
-        self.borderSym = borderSym
+        self.border_sym = border_sym
 
-    def update(ms):
+    def update(self, ms):
         pass
 
-    def draw(tb):
-        for i in range(0, w):
-            for j in range(0, h):
-                tb.change_cell(i, j, self.borderSym, termbox.BLACK, termbox.CYAN);
+    def draw(self, tb):
+        for i in range(0, self.w):
+            for j in range(0, self.h):
+                if i == 0 or i == self.w - 1 or j == 0 or j == self.h -1:
+                    tb.change_cell(self.x + i, self.y + j, 32, termbox.BLACK, self.border_sym);
+                else:
+                    x_center = self.x + self.w/2
+                    text_start_x = int(x_center - len(self.text)/2)
+                    text_end_x = int(x_center + len(self.text)/2)
+                    is_in_text_area_x = i >= text_start_x  and i < text_end_x
+                    y_center = self.y + self.h/2
+                    is_in_text_area_y = j == y_center-1
+                    if is_in_text_area_x and is_in_text_area_y:
+                        tb.change_cell(self.x + i, self.y + j, ord(self.text[i-text_start_x]), termbox.WHITE, termbox.BLACK)
+
+                    
